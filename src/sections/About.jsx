c44e499140bc2329f48c783308a3e6c5df459fb5
Alexander <alexanderfedchin@gmail.com>
2025-05-29 13:03:57 +0300

    Hopefully final changes

--- src/sections/About.jsx
+++ src/sections/About.jsx
@@ -51,24 +51,26 @@ const About = () => {
         </Typography>
       </Box>
 
-      <Box
-        component="img"
-        src="/img/roman-mirko-light.webp"
-        alt="Roman Mirko"
-        loading="lazy"
-        draggable="false"
-        sx={{
-          position: "absolute",
-          bottom: 0,
-          right: isMobile ? "16px" : "5%",
-          width: "auto",
-          height: isMobile ? "300px" : isTablet ? "350px" : "400px",
-          objectFit: "cover",
-          objectPosition: "center",
-          zIndex: -1,
-          userSelect: "none",
-        }}
-      />
+      {!isMobile && (
+        <Box
+          component="img"
+          src="/img/roman-mirko-light.webp"
+          alt="Roman Mirko"
+          loading="lazy"
+          draggable="false"
+          sx={{
+            position: "absolute",
+            bottom: 0,
+            right: isMobile ? "16px" : "5%",
+            width: "auto",
+            height: isMobile ? "300px" : isTablet ? "350px" : "400px",
+            objectFit: "cover",
+            objectPosition: "center",
+            zIndex: -1,
+            userSelect: "none",
+          }}
+        />
+      )}
     </DefaultContainer>
   );
 };
